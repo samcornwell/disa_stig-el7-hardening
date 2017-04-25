b3826758312f1da146803aad6f708f65968961f8
Suftin, Ivan <isuftin@usgs.gov>
2017-04-25 08:36:40 -0500

    - Updated metadata to conform to latest FoodCritic standards - Moved to GPL 3.0 as an OSI approved license. May change again prior to next release - Updated recipes to conform to latest rubocop standards - Updated failing test for boot_settings - Removed OS versioning helper from spec_helper as each test defines its own versioning

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'stig'
 maintainer       'USGS WSI'
 maintainer_email 'isuftin@usgs.gov'
-license          'Public domain'
+license          'GPL-3.0'
 description      'Installs/Configures CIS STIG benchmarks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.6.6'
@@ -14,3 +14,5 @@ supports         'ubuntu'
 
 depends			 'logrotate'
 depends			 'sysctl'
+
+chef_version '~> 13.0.0' if respond_to?(:chef_version)
